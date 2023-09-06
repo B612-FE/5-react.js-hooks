5a6b3e5fb1eaf929514232b03b9ab0e3927c0224
nijyoumis <yuooo26@naver.com>
2023-09-07 02:07:51 +0900

    styled-component 변경

--- src/App.js
+++ src/App.js
@@ -1,7 +1,73 @@
 import { useState } from "react";
-import "./App.css";
-import MessageList from "./MessageList";
-import useToggle from "./useToggle";
+import "./styles/App.css";
+import MessageList from "./components/MessageList";
+import useToggle from "./hooks/useToggle";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Container = styled.div`
+  border: none;
+  padding: 10px;
+  width: 340px;
+  height: 700px;
+  background-color: rgb(218, 233, 241);
+  border-radius: 16px;
+  box-shadow: 0 0 12px 0 rgba(0, 0, 0, 0.04);
+  display: flex;
+  flex-direction: column;
+`;
+
+const ChattingName = styled.header`
+  display: flex;
+  padding: 10px;
+  border-bottom: 1px solid #eee;
+  justify-content: space-between;
+  flex-direction: row;
+`;
+
+const StyledButton = styled.button`
+  flex-basis: 20px;
+  border-radius: 100px;
+  border: 1px;
+`;
+
+const UserName = styled.span`
+  flex-basis: 190px;
+`;
+
+const Chats = styled.section`
+  height: 600px;
+  padding-bottom: 10px;
+`;
+
+const InputForm = styled.form`
+  display: flex;
+  padding: 10px;
+  border-top: 1px solid #eee;
+  position: sticky;
+  bottom: 0;
+`;
+
+const MessageInputBox = styled.input`
+  flex: 1;
+  padding: 8px;
+  border: none;
+  border-radius: 3px;
+`;
+
+const SubmitButton = styled.button`
+  background-color: #f7e730;
+  color: rgb(0, 0, 0);
+  border: none;
+  padding: 8px 12px;
+  border-radius: 3px;
+  cursor: pointer;
+`;
 
 function App() {
   const [chats, setChats] = useState([]);
@@ -24,30 +90,30 @@ function App() {
       setNewChat("");
     }
   };
-
+  //header flex 로 다시 구성하기!!
   return (
-    <div className="App">
-      <div className="Container">
-        <div className="section">
-          <header className="chattingName">
-            <button>&lt;</button>
-            <span onClick={toggle}>{istoMaru ? "Maru🐶" : "Woorie💕"}</span>
-            <button>&#8942;</button>
-          </header>
-          <section className="chats">
-            <MessageList chats={chats} istoMaru={istoMaru}></MessageList>
-          </section>
-          <form onSubmit={addChat} className="input">
-            <input
-              type="text"
-              value={newChat}
-              onChange={(e) => setNewChat(e.target.value)}
-            ></input>
-            <button type="submit">전송</button>
-          </form>
-        </div>
-      </div>
-    </div>
+    <Wrapper>
+      <Container>
+        <ChattingName>
+          <StyledButton>&lt;</StyledButton>
+          <UserName onClick={toggle}>
+            {istoMaru ? "Maru🐶" : "Woorie💕"}
+          </UserName>
+          <StyledButton>&#8942;</StyledButton>
+        </ChattingName>
+        <Chats>
+          <MessageList chats={chats} istoMaru={istoMaru}></MessageList>
+        </Chats>
+        <InputForm onSubmit={addChat} className="input">
+          <MessageInputBox
+            type="text"
+            value={newChat}
+            onChange={(e) => setNewChat(e.target.value)}
+          ></MessageInputBox>
+          <SubmitButton type="submit">전송</SubmitButton>
+        </InputForm>
+      </Container>
+    </Wrapper>
   );
 }
 
