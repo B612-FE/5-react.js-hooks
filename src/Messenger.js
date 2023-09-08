dc0a628ca8fc69028d0d1cadaa08b7dc7708742c
Kim_Nayeon <giant18s@naver.com>
2023-09-08 15:12:26 +0900

    채팅방

--- src/Messenger.js
+++ src/Messenger.js
@@ -1,52 +1,56 @@
 // Messenger.js
 import React, { useState } from 'react';
-import useToggle from './useToggle';//useToggle커스텀 훅을 불러오기
-import './Messenger.css';//스타일링 css파일 불러오기
+import './Messenger.css';
 
 function Messenger() {
-  const [isMessengerOpen, toggleMessenger] = useToggle(false);//토글기능을 이용한 끄고키기
-  const [message, setMessage] = useState('');//현제 입력된 메시지 상태
-  const [messages, setMessages] = useState([]);//이전에 보낸 메시지
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+  const [isUserSender, toggleUserSender] = useState(true); // 사용자가 발신자인지 여부를 토글
 
-  const handleSendMessage = () => {//메세지 보내기 위한 함수 제작
+  const handleSendMessage = () => {
     if (message.trim() !== '') {
-      setMessages(prevMessages => [...prevMessages, { text: message, sender: 'user' }]);
-      setMessage('');//입력 상자 비우기
+      const sender = isUserSender ? 'user' : 'bot';
+      setMessages(prevMessages => [...prevMessages, { text: message, sender }]);
+      setMessage('');
     }
+  };
+
+  const handleToggleSender = () => {
+    toggleUserSender(prevIsUserSender => !prevIsUserSender); // 발신자를 토글
   };
 
   return (
     <div className="messenger-container">
-      <button className="toggle-button" onClick={toggleMessenger}>
-        Toggle Messenger
-      </button>
-      {isMessengerOpen && (
-        <div className="messenger">
-          <div className="messages">
-            {messages.map((msg, index) => (
-              <div
-                key={index}
-                className={`message-container ${
-                  msg.sender === 'user' ? 'user' : 'bot'
-                }`}
-              >
-                <div className="message">{msg.text}</div>
+      <div className="messenger">
+        <div className="messages">
+          {messages.map((msg, index) => (
+            <div
+              key={index}
+              className={`message-container ${
+                msg.sender === 'user' ? 'user' : 'bot'
+              }`}
+            >
+              <div className={`message ${msg.sender === 'user' ? 'user' : 'bot'}`}>
+                {msg.text}
               </div>
-            ))}
-          </div>
-          <div className="input-container">
-            <input
-              type="text"
-              placeholder="Type a message..."
-              value={message}
-              onChange={e => setMessage(e.target.value)}
-            />
-            <button className="send-button" onClick={handleSendMessage}>
-              Send
-            </button>
-          </div>
+            </div>
+          ))}
         </div>
-      )}
+        <div className="input-container">
+          <input
+            type="text"
+            placeholder="Type a message..."
+            value={message}
+            onChange={e => setMessage(e.target.value)}
+          />
+          <button className="send-button" onClick={handleSendMessage}>
+            Send
+          </button>
+          <button className="toggle-sender-button" onClick={handleToggleSender}>
+            Toggle Sender
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
--- src/useToggle.js
+++ src/useToggle.js
@@ -1,15 +1,14 @@
+// useToggle.js
 import { useState } from 'react';
-//커스텀 훅 useToggle을 정의, 초기값없다면 기본으로 false
+
 function useToggle(initialValue = false) {
-    //useState사용하여 value상태와 해당상태 변경함수 setValue선언
     const [value, setValue] = useState(initialValue);
 
-    //toggle함스 정의 value 상태를 토글하는 기능 수행
     const toggle = () => {
     setValue(prevValue => !prevValue);
-};
+    };
 
-    return [value, toggle];//value상태와 toggle함수 반환
+    return [value, toggle];
 }
 
 export default useToggle;
